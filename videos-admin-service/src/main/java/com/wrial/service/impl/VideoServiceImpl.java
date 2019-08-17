e1fe391f27097bba4ab3110809e738a042e07953
wrail <2806935450@qq.com>
2019-08-17 22:16:35 +0800

    完成给BGM增加删除提供Zookeeper支持，使得小程序后端和后台管理同步

--- videos-admin-service/src/main/java/com/wrial/service/impl/VideoServiceImpl.java
+++ videos-admin-service/src/main/java/com/wrial/service/impl/VideoServiceImpl.java
@@ -8,13 +8,16 @@ import com.wrial.mapper.VideosMapper;
 import com.wrial.pojo.Bgm;
 import com.wrial.pojo.BgmExample;
 import com.wrial.service.VideoService;
+import com.wrial.utils.JsonUtils;
 import com.wrial.utils.PagedResult;
 import enums.BGMOperatorTypeEnum;
 import org.n3r.idwroker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class VideoServiceImpl implements VideoService {
@@ -31,13 +34,22 @@ public class VideoServiceImpl implements VideoService {
     @Autowired
     private ZKCurator zkCurator;
 
+    /*
+    为什么使用Map呢？
+    1.简化小程序后端的字符串切割（如果使用字符串转为Json格式，需要切割字符串）
+    2.在删除操作时，如果没有预先保存，是在后边是查不到的，因此使用Map也能更简单的保存值，并传递到小程序后端
+     */
     @Override
     public void addBgm(Bgm bgm) {
         String bgmId = sid.nextShort();
         bgm.setId(bgmId);
         bgmMapper.insert(bgm);
-        zkCurator.init();
-        zkCurator.sendBgmOperator(bgmId, BGMOperatorTypeEnum.ADD.value);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("operType", BGMOperatorTypeEnum.ADD.type);
+        map.put("path", bgm.getPath());
+
+        zkCurator.sendBgmOperator(bgmId, JsonUtils.objectToJson(map));
     }
 
     @Override
@@ -63,6 +75,12 @@ public class VideoServiceImpl implements VideoService {
      */
     @Override
     public void deleteBgm(String id) {
+        Bgm bgm = bgmMapper.selectByPrimaryKey(id);
+        Map<String, String> map = new HashMap<>();
+        map.put("operType", BGMOperatorTypeEnum.DELETE.type);
+        map.put("path", bgm.getPath());
+
+        zkCurator.sendBgmOperator(id, JsonUtils.objectToJson(map));
         bgmMapper.deleteByPrimaryKey(id);
 
     }
